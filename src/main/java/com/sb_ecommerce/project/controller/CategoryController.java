4a7a2ac3afc97060e6f368ac071b194226f28720
chirag-2641 <chirag2641@gmail.com>
2025-08-04 01:25:52 +0530

    Inital setup

---
+++ src/main/java/com/sb_ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,19 @@
+package com.sb_ecommerce.project.controller;
+
+import com.sb_ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categories;
+    }
+}
