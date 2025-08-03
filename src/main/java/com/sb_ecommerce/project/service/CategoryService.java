f6c3d3bacd641542c91f6b69e6204ab248bfd2cf
chirag-2641 <chirag2641@gmail.com>
2025-08-04 01:39:32 +0530

    Updating Category module

---
+++ src/main/java/com/sb_ecommerce/project/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.sb_ecommerce.project.service;
+
+import com.sb_ecommerce.project.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
+}
