bb64765f0edba54642f08e69ccbc95ad44d4ea8c
chirag-2641 <chirag2641@gmail.com>
2025-08-05 21:31:27 +0530

    Add custom validations to the code

---
+++ src/main/java/com/sb_ecommerce/project/exceptions/APIException.java
@@ -0,0 +1,12 @@
+package com.sb_ecommerce.project.exceptions;
+
+public class APIException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public APIException() {
+    }
+
+    public APIException(String message) {
+        super(message);
+    }
+}
