3eba0cdbdc5853f915efdce6c6956d6609adeb2b
chirag-2641 <chirag2641@gmail.com>
2025-08-15 13:31:33 +0530

    Add payment & order module

---
+++ src/main/java/com/sb_ecommerce/project/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.sb_ecommerce.project.service;
+
+import com.sb_ecommerce.project.payload.OrderDTO;
+import jakarta.transaction.Transactional;
+
+public interface OrderService {
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
